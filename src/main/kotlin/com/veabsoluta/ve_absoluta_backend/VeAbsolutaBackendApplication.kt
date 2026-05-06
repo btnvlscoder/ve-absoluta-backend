7122a86e988dec11b3d3f7a87bb6e1fbf361d31e
btnvlscoder <btnvlscoder@gmail.com>
2026-05-06 03:26:15 -0400

    test: prueba de vida del servidor en Render

--- src/main/kotlin/com/veabsoluta/ve_absoluta_backend/VeAbsolutaBackendApplication.kt
+++ src/main/kotlin/com/veabsoluta/ve_absoluta_backend/VeAbsolutaBackendApplication.kt
@@ -2,10 +2,19 @@ package com.veabsoluta.ve_absoluta_backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class VeAbsolutaBackendApplication
+@RestController // Convertimos la clase principal en un controlador de prueba
+class VeAbsolutaBackendApplication {
+
+    @GetMapping("/ping")
+    fun ping(): String {
+        return "¡El backend está vivo y Render sí actualiza, Bastián!"
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<VeAbsolutaBackendApplication>(*args)
+    runApplication<VeAbsolutaBackendApplication>(*args)
 }
