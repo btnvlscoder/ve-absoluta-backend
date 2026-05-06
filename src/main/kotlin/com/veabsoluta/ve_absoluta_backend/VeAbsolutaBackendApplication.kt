f1b8e88231ee44864230522ce40bfc0277a3be05
btnvlscoder <btnvlscoder@gmail.com>
2026-05-06 04:01:44 -0400

    fix: forzar Component Scan absoluto y test de ping

--- src/main/kotlin/com/veabsoluta/ve_absoluta_backend/VeAbsolutaBackendApplication.kt
+++ src/main/kotlin/com/veabsoluta/ve_absoluta_backend/VeAbsolutaBackendApplication.kt
@@ -2,23 +2,10 @@ package com.veabsoluta.ve_absoluta_backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
-// Forzamos a Spring a escanear desde la raíz absoluta
-@SpringBootApplication(scanBasePackages = [
-    "com.veabsoluta", 
-    "com.veabsoluta.ve_absoluta_backend"
-])
-@RestController
-class VeAbsolutaBackendApplication {
-
-    @GetMapping("/ping")
-    fun ping(): String {
-        return "¡El backend está vivo y el radar funciona!"
-    }
-}
+@SpringBootApplication
+class VeAbsolutaBackendApplication
 
 fun main(args: Array<String>) {
-    runApplication<VeAbsolutaBackendApplication>(*args)
+	runApplication<VeAbsolutaBackendApplication>(*args)
 }
