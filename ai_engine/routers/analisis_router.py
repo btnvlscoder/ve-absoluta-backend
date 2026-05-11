f27e39f890157d57cd4852cd576e03e0180cea32
btnvlscoder <btnvlscoder@gmail.com>
2026-05-11 02:14:51 -0400

    feat(forense): implementacion de filtro Laplaciano SRM para detectar huella optica de hardware y evitar falsos positivos por edicion profesional

--- ai_engine/routers/analisis_router.py
+++ ai_engine/routers/analisis_router.py
@@ -4,7 +4,7 @@ import requests
 from io import BytesIO
 from PIL import Image
 
-from services.forensic_service import realizar_analisis_ela, generar_narrativa_ela
+from services.forensic_service import realizar_analisis_ela, generar_narrativa_ela, extraer_huella_sensor
 from services.vit_service import analizar_con_vit, generar_narrativa_vit
 
 router = APIRouter()
@@ -24,7 +24,6 @@ def _descargar_imagen(url: str) -> Image.Image:
 async def analisis_pericial_completo(peticion: PeticionImagen):
     imagen = _descargar_imagen(peticion.url)
     
-    # 1. Ejecutamos ambos análisis
     res_vit = analizar_con_vit(imagen)
     res_ela = realizar_analisis_ela(imagen)
 
@@ -33,7 +32,6 @@ async def analisis_pericial_completo(peticion: PeticionImagen):
     if "error" in res_ela:
         raise HTTPException(status_code=500, detail=res_ela["detalle"])
 
-    # 2. Extraer datos para la narrativa
     veredicto = res_vit["prediccion"]
     confianza = res_vit["confianza"]
     matriz_atencion = res_vit["grid_attn"]
@@ -41,11 +39,13 @@ async def analisis_pericial_completo(peticion: PeticionImagen):
     dif_max = res_ela["metricas"]["diferencia_maxima"]
     ruido_prom = res_ela["metricas"]["ruido_promedio"]
 
-    # 3. Generar la narrativa dinámica
-    texto_vit = generar_narrativa_vit(veredicto, confianza, matriz_atencion)
-    texto_ela = generar_narrativa_ela(dif_max, ruido_prom)
+    # 🚀 NUEVO: Extraemos la huella física del lente de la cámara
+    varianza_sensor = extraer_huella_sensor(imagen)
 
-    # 4. Retornar el Súper JSON
+    # Le pasamos la varianza a la narrativa para que decida inteligentemente
+    texto_vit = generar_narrativa_vit(veredicto, confianza, matriz_atencion)
+    texto_ela = generar_narrativa_ela(dif_max, ruido_prom, varianza_sensor) # <--- OJO AQUÍ
+
     return {
         "veredicto_final": veredicto,
         "confianza_global": confianza,
@@ -62,6 +62,6 @@ async def analisis_pericial_completo(peticion: PeticionImagen):
         },
         "metadata": {
             "sistema": "VE ABSOLUTA Enterprise",
-            "version": "2.2.0-XAI"
+            "version": "2.3.0-SRM" # Actualizamos versión
         }
     }
